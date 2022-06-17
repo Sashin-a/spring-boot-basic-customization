f3563f9ecd8c66a4820b34142a71ed7250a9f0d6
Sashin Agrawal <sashin_agrawal@epam.com>
2022-06-17 14:53:57 +0530

    spring boot customization

---
+++ src/main/java/com/springboot/customization/bootcustomfilters/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.springboot.customization.bootcustomfilters.controller;
+
+import com.springboot.customization.bootcustomfilters.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Rest controller for User
+ * @author hemant
+ *
+ */
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+	private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
+
+	@GetMapping("/fetch-user")
+	public List<User> getAllUsers() {
+		LOG.info("Fetching all the users");
+		return Arrays.asList(
+				new User(UUID.randomUUID().toString(), "User1", "user1@test.com"),
+				new User(UUID.randomUUID().toString(), "User2", "user2@test.com"),
+				new User(UUID.randomUUID().toString(), "User3", "user3@test.com"));
+	}
+
+	@GetMapping("/user2")
+	public List<User> getAllUsers1() {
+		LOG.info("Fetching all the users");
+		return Arrays.asList(
+				new User(UUID.randomUUID().toString(), "User1", "user1@test.com"),
+				new User(UUID.randomUUID().toString(), "User2", "user2@test.com"),
+				new User(UUID.randomUUID().toString(), "User3", "user3@test.com"));
+	}
+
+}
